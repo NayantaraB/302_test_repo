08c46c19908fe1b9f94dca303838498555583609
NayantaraB <59986856+NayantaraB@users.noreply.github.com>
2020-01-21 11:40:42 -0500

    Add files via upload

---
+++ Nayantara_ Biswas_Lab 1.py
@@ -0,0 +1,16 @@
+"""
+Author: Nayantara Biswas
+Created: 21 January 2019
+Note: This is on Python 3.4"""
+
+pi = 3.14 #2 digits kept
+r = 2.0
+
+#This line calculates area
+area = pi *(r**2)
+#This line calculates perimeter
+perim = 2.0*pi*r
+#These lines gives the output and results
+print ('Given r:',r)
+print ('Area is:', area)
+print ('Given Perimeter is:', perim)
